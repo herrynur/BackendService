1f0f5708ddeb7433c4b20faac553638648047b95
Heri Nur A <herinuraliim@gmail.com>
2024-03-08 21:01:40 +0700

    feat (mobile) : add mobile service

---
+++ MobileService/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MobileService
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MobileService/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MobileService
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
